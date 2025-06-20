a93bdadc2e8fcedb546b316989833408fd8593d3
nishanth0004 <nishanthpallakadu6699@gmail.com>
2025-06-20 12:04:43 +0530

    commit json

--- json.js
+++ json.js
@@ -8,7 +8,7 @@ const jsObject = {
 
 //JavaScript Object Notation
 let jsonObject = {
-    name: "nishanth",
+    name: "HP",
     age: 25,
 };
 //methods(functions inside objects) are not supported
@@ -20,3 +20,38 @@ console.log(typeof stringJSON);
 let normalJSON = JSON.parse(stringJSON);
 console.log(normalJSON);
 console.log(typeof normalJSON);
+
+// const jsonbject = {
+//     name: "John doe",
+//     age: 25,
+//     display: function(){
+//         console.log("Hello");
+//     },
+// };
+
+// let jsonObject = {
+//     "name": "John Doe",
+//     "age": 25,
+// };
+// //function inside object (methods) not supported
+
+
+// let stringJSON = JSON.stringify(jsonObject);
+// console.log(stringJSON);
+// console.log(typeof stringJSON);
+
+// let normalJSON = JSON.parse(stringJSON);
+// console.log(normalJSON);
+// console.log(typeof normalJSON);
+
+
+
+localStorage.setItem("user.name", "Martin Luthor");
+let item = localStorage.getItem("username");
+console.log(item);
+
+localStorage.removeItem("user name", "Martin Luthor");
+let item2 = localStorage.getItem("username");
+console.log(item2);
+
+sessionStorage.setItem("user name", "Martin Luthor")
